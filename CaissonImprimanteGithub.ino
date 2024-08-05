f7915ab85d60f2b9bf24c15de2bc315089041941
Benjamin Icart <152204110+BenjaminIcart@users.noreply.github.com>
2024-08-05 16:58:22 +0200

    Update CaissonImprimanteGithub.ino
    Ajout CSS à la page + Ajout fonctionnalité de changer le programme via le wifi + Ajout de sécurisation sur les pages importantes

--- CaissonImprimanteGithub.ino
+++ CaissonImprimanteGithub.ino
@@ -2,14 +2,15 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>   //#
 #include "DHT.h"
 #include <EEPROM.h>
 
+
 WiFiClient client; 
 
-char* ssid = "YOUR_SSID"; // votre SSID
-char* password = "YOUR_PASSWORD"; // votre mot de passe wifi
-const char* host = "maker.ifttt.com";
+const char* ssid = "YOUR_SSID"; // votre SSID
+const char* password = "YOUR_PASSWORD"; // votre mot de passe wifi
 MDNSResponder mdns; 
 long port = 3100;
 ESP8266WebServer server(port); // serveur HTTP
@@ -23,6 +24,12 @@ float h;
 String Etat = "...";
 int temperatureMax = 0;  // Valeur à stocker
 String UrlVideo = "http://X.X.X.X:";
+
+const char* Loginauth = "YOUR_LOGIN";
+const char* Passwordauth = "YOUR_PASSWORD";
+ESP8266HTTPUpdateServer httpUpdater(true);
+
+
 
 void writeStringToEEPROM(int addr, const String &str) {
     int len = str.length();
@@ -47,39 +54,112 @@ String readStringFromEEPROM(int addr) {
 
 
 void handleRoot() {
-    String html = "<html><body>";
-    html += "<h1>Temp&eacuterature et humidit&eacutee actuelles</h1>";
-    html += "<p>Temp&eacuterature: " + String(t) + "</p>";
-    html += "<p>Humidit&eacutee: " + String(h) + "</p>";
 
-    html += "<h1>Temperature Max</h1>";
-    html += "<p>Temp&eacuterature actuelle max: " + String(temperatureMax) + "</p>";
-    html += "<form action='/setTemperatureMax' method='GET'>";
-    html += "Temp&eacuterature Max: <input type='number' name='value'>";
-    html += "<input type='submit' value='Set'>";
-    html += "</form>";
-
-    html += "<h1>Url Video</h1>";
-    html += "<p>Url vid&eacuteo actuelle: " + UrlVideo + "</p>";
-    html += "<form action='/setUrlVideo' method='GET'>";
-    html += "Url Video: <input type='text' name='url'>";
-    html += "<input type='submit' value='Set'>";
-    html += "</form>";
-
-    html += "<h1>Vid&eacuteo Stream</h1>";
-    html += "<img style='display: block; -webkit-user-select: none; margin: auto; background-color: hsl(0, 0%, 25%);' ";
-    html += "src='" + UrlVideo + "' width='1065' height='599'>";
-    html += "</body></html>";
-
+  if (!server.authenticate(Loginauth, Passwordauth)){
+    return server.requestAuthentication();
+  }
+    String html = "<!DOCTYPE html>"
+                  "<html lang='fr'>"
+                  "<head>"
+                  "<meta charset='UTF-8'>"
+                  "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+                  "<title>Contrôle de Température et Vidéo</title>"
+                  "<link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap' rel='stylesheet'>"
+                  "<style>"
+                  "body {"
+                  "    font-family: 'Poppins', sans-serif;"
+                  "    margin: 0;"
+                  "    padding: 0;"
+                  "    background-color: #ffff;"
+                  "    color: #333;"
+                  "}"
+                  ".container {"
+                  "    max-width: 800px;"
+                  "    margin: 0 auto;"
+                  "    padding: 20px;"
+                  "}"
+                  "h1 {"
+                  "    font-size: 2.5rem;"
+                  "    color: #007bff;"
+                  "    text-align: center;"
+                  "    margin-bottom: 20px;"
+                  "}"
+                  "p {"
+                  "    font-size: 1.25rem;"
+                  "    text-align: center;"
+                  "    margin: 10px 0;"
+                  "}"
+                  "form {"
+                  "    text-align: center;"
+                  "    margin: 20px 0;"
+                  "}"
+                  "input[type='number'],"
+                  "input[type='text'] {"
+                  "    font-size: 1rem;"
+                  "    padding: 10px;"
+                  "    margin: 5px;"
+                  "    border: 1px solid #ddd;"
+                  "    border-radius: 5px;"
+                  "    width: 200px;"
+                  "}"
+                  "input[type='submit'] {"
+                  "    font-size: 1rem;"
+                  "    padding: 10px 20px;"
+                  "    border: none;"
+                  "    border-radius: 5px;"
+                  "    background-color: #007bff;"
+                  "    color: #fff;"
+                  "    cursor: pointer;"
+                  "    transition: background-color 0.3s ease;"
+                  "}"
+                  "input[type='submit']:hover {"
+                  "    background-color: #0056b3;"
+                  "}"
+                  "img {"
+                  "    display: block;"
+                  "    margin: 20px auto;"
+                  "    max-width: 100%;"
+                  "    border: 2px solid #ddd;"
+                  "    border-radius: 10px;"
+                  "}"
+                  "</style>"
+                  "</head>"
+                  "<body>"
+                  "<div class='container'>"
+                  "<h1>Température et Humidité</h1>"
+                  "<p>Température: " + String(t) + "°C</p>"
+                  "<p>Humidité: " + String(h) + "%</p>"
+                  "<h1>Température Max</h1>"
+                  "<p>Température actuelle max: " + String(temperatureMax) + "°C</p>"
+                  "<form action='/setTemperatureMax' method='GET'>"
+                  "Température Max:"
+                  "<input type='number' name='value'>"
+                  "<input type='submit' value='Définir'>"
+                  "</form>"
+                  "<h1>Url Vidéo</h1>"
+                  "<p>Url vidéo actuelle: <a href='" + UrlVideo + "' target='_blank'>" + UrlVideo + "</a></p>"
+                  "<form action='/setUrlVideo' method='GET'>"
+                  "Url Vidéo:"
+                  "<input type='text' name='url'>"
+                  "<input type='submit' value='Définir'>"
+                  "</form>"
+                  "<h1>Vidéo Stream</h1>"
+                  "<img src='" + UrlVideo + "' alt='Stream Video'>"
+                  "</div>"
+                  "</body>"
+                  "</html>";
     server.send(200, "text/html", html);
 }
 
 
 
 void handleSetTemperatureMax() {
+    if (!server.authenticate(Loginauth, Passwordauth)){
+      return server.requestAuthentication();
+    }
     if (server.hasArg("value")) {
         int newTemperatureMax = server.arg("value").toInt();
-        if (newTemperatureMax > 0) {
+        if (newTemperatureMax > 0) { // validation simple
             temperatureMax = newTemperatureMax;
             EEPROM.write(0, temperatureMax);  // Stocker dans l'EEPROM à l'adresse 0
             EEPROM.commit();
@@ -91,6 +171,9 @@ void handleSetTemperatureMax() {
 }
 
 void handleSetUrlVideo() {
+    if (!server.authenticate(Loginauth, Passwordauth)){
+      return server.requestAuthentication();
+    }
     if (server.hasArg("url")) {
         String newUrlVideo = server.arg("url");
         writeStringToEEPROM(1, newUrlVideo);  // Stocker à l'adresse 1 (en évitant l'adresse 0)
@@ -118,6 +201,7 @@ void handleHumidite()
 {
     server.send(200, "text/plain", String(h,1));
 }
+
 
 
 void connect(char *_SSID, char* _PWD) 
@@ -164,28 +248,38 @@ void setup() {
     pinMode(relayPin2, OUTPUT);
     dht.begin();
 
-    // Serveur web configuration
+    // Connexion au Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to WiFi");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Configuration OTA
+    if (!MDNS.begin("esp8266")) {
+        Serial.println("Error setting up MDNS responder!");
+    }
+    httpUpdater.setup(&server, "/update", Loginauth, Passwordauth);
+    server.begin();
+    Serial.println("HTTP server started");
+
+    // Routes pour le serveur web
     server.on("/", handleRoot);
     server.on("/etat", handleEtat);
     server.on("/temperature", handleTemp);
     server.on("/humidite", handleHumidite);
-    server.on("/setTemperatureMax", handleSetTemperatureMax); // Nouvelle route
+    server.on("/setTemperatureMax", handleSetTemperatureMax);
     server.on("/setUrlVideo", handleSetUrlVideo);
 
-    connect(ssid, password); // connexion au reseau Wifi
-
-    if (mdns.begin("esp8266", WiFi.localIP())) {
-        Serial.println("MDNS responder started");
-    }
-    server.begin();
-    Serial.println("HTTP server started");
-
-    // Lire la valeur stockée dans l'EEPROM
+    // Lire les valeurs de l'EEPROM
     temperatureMax = EEPROM.read(0);
     Serial.print("Initial temperatureMax: ");
     Serial.println(temperatureMax);
 
-    UrlVideo = readStringFromEEPROM(1); // Lire l'URL stockée à partir de l'adresse 1
+    UrlVideo = readStringFromEEPROM(1);
     Serial.print("Stored UrlVideo: ");
     Serial.println(UrlVideo);
 }
@@ -193,7 +287,8 @@ void setup() {
 
  
 void loop() {
-    server.handleClient(); // gestion du serveur
+    server.handleClient(); // Traitement des requêtes HTTP
+
     h = dht.readHumidity();
     t = dht.readTemperature();
     Serial.print("Humidity: ");
@@ -212,3 +307,4 @@ void loop() {
         Etat = "Allumés";
     }
 }
+
